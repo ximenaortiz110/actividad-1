e91d7e54168b69e698fd4cdd71c674806bc453c4
XIMENAORTIZ2 <85369235+ximenaortiz110@users.noreply.github.com>
2021-06-10 19:30:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    double num1 = 34.6;
+    double num2 = 65.4;
+    double resultado = 0;
+     resultado = num1+num2;
+    Console.WriteLine ("la suma de los dos valores es de: "+ resultado);
+
+  }
+}
